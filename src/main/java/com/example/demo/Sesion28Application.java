eef88b201d0584a3a5cf77bf5189614b23dd99aa
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-06-12 14:15:58 -0400

    Agregacion de springboot al proyecto

---
+++ src/main/java/com/example/demo/Sesion28Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion28Application.class, args);
+	}
+
+}
